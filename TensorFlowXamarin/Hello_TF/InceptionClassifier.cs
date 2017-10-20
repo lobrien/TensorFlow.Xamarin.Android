18114bc8ff5176823be15dc249e319cc0392c1e0
lobrien <lobrien@knowing.net>
2017-10-20 10:24:00 -1000

    Runs, but not LSTM

---
+++ TensorFlowXamarin/Hello_TF/InceptionClassifier.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Android.App;
+using Android.Content.Res;
+using Android.Graphics;
+using Org.Tensorflow.Contrib.Android;
+
+namespace Hello_TF
+{
+
+	public struct Recognition
+	{
+		public string Label;
+		public float Confidence;
+
+		public override string ToString()
+		{
+			return string.Format($"[Label: {Label}, Confidence: {Confidence}]");
+		}
+	}
+
+	public class InceptionClassifier : Activity
+	{
+		const int INPUT_SIZE = 224;
+		const int IMAGE_MEAN = 117;
+		const float IMAGE_STD = 1;
+		const string INPUT_NAME = "input";
+		const string OUTPUT_NAME = "output";
+
+		const string MODEL_FILE = "file:///android_asset/tensorflow_inception_graph.pb";
+		const string LABEL_FILE = "imagenet_comp_graph_label_strings.txt";
+
+		List<String> labels = new List<string>();
+		TensorFlowInferenceInterface inferenceInterface;
+
+		long OutputSize()
+		{
+			return inferenceInterface.GraphOperation(OUTPUT_NAME).Output(0).Shape().Size(1);
+		}
+
+
+		public InceptionClassifier(AssetManager assetManager)
+		{
+			labels.AddRange(ReadLabelsIntoMemory(assetManager, LABEL_FILE));
+			inferenceInterface = new TensorFlowInferenceInterface(assetManager, MODEL_FILE);
+			var operation = inferenceInterface.GraphOperation(OUTPUT_NAME);
+			var numClasses = operation.Output(0).Shape().Size(1);
+			//TODO: Confirm in Android, because my results are 1001 labels, 1008 classes
+			System.Console.WriteLine($"Read {labels.Count} labels, output layer size is {numClasses}");
+			var bmp = BitmapCreate(assetManager, "husky.png");
+			var results = Recognize(bmp);
+		}
+
+		static Bitmap BitmapCreate(AssetManager assetManager, string assetName)
+		{
+			using (var str = assetManager.Open(assetName))
+			{
+				return BitmapFactory.DecodeStream(str);
+			}
+		}
+
+		IEnumerable<String> ReadLabelsIntoMemory(AssetManager assets, string fileName)
+		{
+			using (StreamReader sr = new StreamReader(assets.Open(fileName)))
+			{
+				var content = sr.ReadToEnd();
+				return content.Split(new[] { '\n' });
+			}
+		}
+
+		public IEnumerable<Recognition> Recognize(Bitmap bitmap)
+		{
+			var argbPixelArray = new int[INPUT_SIZE * INPUT_SIZE];
+			bitmap.GetPixels(argbPixelArray, 0, bitmap.Width, 0, 0, bitmap.Width, bitmap.Height);
+
+			var normalizedPixelComponents = new float[argbPixelArray.Length * 3];
+			for (int i = 0; i < argbPixelArray.Length; i++)
+			{
+				var argb = argbPixelArray[i];
+				normalizedPixelComponents[i * 3 + 0] = (((argb >> 16) & 0xFF) - IMAGE_MEAN) / IMAGE_STD;
+				normalizedPixelComponents[i * 3 + 1] = (((argb >> 8) & 0xFF) - IMAGE_MEAN) / IMAGE_STD;
+				normalizedPixelComponents[i * 3 + 2] = (((argb) & 0xFF) - IMAGE_MEAN) / IMAGE_STD;
+			}
+
+			// Copy the input data into TF
+			inferenceInterface.Feed(INPUT_NAME, normalizedPixelComponents, 1, INPUT_SIZE, INPUT_SIZE, 3);
+
+			// Run the inference
+			inferenceInterface.Run(new[] { OUTPUT_NAME });
+
+			// Grab the output data 
+			var outputs = new float[OutputSize()];
+			inferenceInterface.Fetch(OUTPUT_NAME, outputs);
+			var results = new Recognition[labels.Count];
+			for (int i = 0; i < labels.Count; i++)
+			{
+				results[i] = new Recognition { Confidence = outputs[i], Label = labels[i] };
+			}
+			// Sort high-to-low via confidence 
+			Array.Sort(results,(x, y) => y.Confidence.CompareTo(x.Confidence));
+			System.Console.WriteLine(results[0]);
+			return results;
+		}
+	}
+}
---
+++ TensorFlowXamarin/Hello_TF/MainActivity.cs
@@ -0,0 +1,118 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Org.Tensorflow.Contrib.Android;
+using Android.Graphics;
+using Android.Content.Res;
+using System.Linq;
+using System;
+using Org.Tensorflow;
+using System.IO;
+
+namespace Hello_TF
+{
+	[Activity(Label = "Hello_TF", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		long byteHash(byte[] contents)
+		{
+			long hash = 0;
+			for (int i = 0; i < contents.Length; i++)
+			{
+				hash += contents[i];
+			}
+			return hash;
+		}
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button>(Resource.Id.myButton);
+
+			button.Click += delegate { button.Text = $"{count++} clicks!"; };
+
+			try
+			{
+				Java.Lang.JavaSystem.LoadLibrary("tensorflow_inference");
+				var r = new RunStats();
+				var g = new Graph();
+				var sess = new Session(g);
+				var runner = sess.InvokeRunner();
+				byte[] content = null;
+				using (var f = Assets.Open("tensorflow_inception_graph.pb"))
+				using (StreamReader sr = new StreamReader(f))
+				using(var ms = new MemoryStream())
+				{
+					sr.BaseStream.CopyTo(ms);
+					content = ms.ToArray();
+				}
+				long hash = byteHash(content);
+				g.ImportGraphDef(content);
+
+				TensorFlowInferenceInterface tfi = new TensorFlowInferenceInterface(Assets, "file:///android_asset/TF_LSTM_Inference.pb");
+				float[] inputSeaLevels = new float[] {
+		  4.92F, 2.022F, -0.206F, 2.355F, 4.08F, 1.828F, -0.005F, 2.83F,
+				  4.966F, 2.715F, -0.073F, 1.69F, 3.958F, 2.5F, 0.201F, 2.075F, 4.754F, 3.475F,
+				  0.345F, 0.954F, 3.665F, 3.165F, 0.562F, 1.285F, 4.415F, 4.083F, 0.83F, 0.327F,
+				  3.304F, 3.589F, 0.976F, 0.707F, 3.989F, 4.37F, 1.375F, 0.039F, 2.863F, 3.715F,
+				  1.525F, 0.507F, 3.403F, 4.38F, 2.06F, 0.097F, 2.251F, 3.671F, 2.223F, 0.579F,
+				  2.605F, 4.25F, 2.791F, 0.306F, 1.489F, 3.575F, 2.907F, 0.759F, 1.741F, 4.018F,
+				  3.381F, 0.589F, 0.796F, 3.382F, 3.441F, 1.103F, 1.099F, 3.566F, 3.75F, 1.09F,
+				  0.393F, 2.906F, 3.831F, 1.77F, 0.816F, 2.739F, 3.922F, 1.891F, 0.285F, 2.02F,
+				  4.083F, 2.727F, 0.833F, 1.593F, 3.863F, 2.824F, 0.408F, 0.906F, 4.071F, 3.738F,
+				  1.149F, 0.511F, 3.431F, 3.626F, 0.863F, -0.004F, 3.57F, 4.568F, 1.906F,
+				  -0.079F, 2.489F, 4.115F, 1.777F, -0.396F, 2.456F, 5.046F, 3.106F, -0.04F,
+				  1.113F, 4.168F, 2.998F, -0.246F, 0.941F, 5.009F, 4.425F, 0.516F, -0.267F,
+				  3.695F, 4.092F, 0.405F, -0.384F, 4.32F, 5.429F, 1.541F, -1.036F, 2.628F,
+				  4.711F, 1.479F, -0.977F, 3.043F, 5.82F, 2.856F, -0.932F, 1.249F, 4.713F,
+				  2.828F, -0.734F, 1.438F, 5.546F, 4.182F, -0.159F, -0.109F, 4.137F, 4.03F,
+				  0.09F, 0.042F, 4.693F, 5.075F, 0.921F, -0.925F, 3.14F, 4.67F, 1.227F, -0.599F,
+				  3.458F, 5.274F, 2.089F, -0.893F, 1.945F, 4.618F, 2.489F, -0.336F, 2.106F,
+				  4.828F, 3.178F, -0.192F, 0.816F, 4.026F, 3.61F, 0.491F, 0.961F, 3.972F, 3.93F,
+				  0.717F, 0.057F, 3.155F, 4.273F, 1.457F, 0.369F, 2.96F, 4.147F, 1.559F, -0.088F,
+				  2.226F, 4.349F, 2.398F, 0.485F, 1.964F, 3.861F, 2.329F, 0.335F, 1.357F, 3.972F,
+				  3.286F, 1.092F, 1.083F, 3.277F, 3.011F, 0.981F, 0.632F, 3.375F, 3.987F
+	  };
+				String INPUT_ARGUMENT_NAME = "lstm_1_input";
+				String OUTPUT_VARIABLE_NAME = "output_node0";
+				int OUTPUT_SIZE = 100;
+
+				tfi.Feed(INPUT_ARGUMENT_NAME, inputSeaLevels, inputSeaLevels.Length, 1, 1);
+				tfi.Run(new String[] { OUTPUT_VARIABLE_NAME });
+				float[] predictions = new float[OUTPUT_SIZE];
+				tfi.Fetch(OUTPUT_VARIABLE_NAME, predictions);
+
+			}
+			catch (Exception x)
+			{
+				Console.WriteLine(x);
+			}
+
+			// Seriously, it's this easy?
+			using (var ic = new InceptionClassifier(BaseContext.Assets))
+			{
+				var results = ic.Recognize(BitmapCreate(BaseContext.Assets, "husky.png"));
+				var top = results.First();
+				//				TextView label = FindViewById<TextView>(Resource.Id.)
+			}
+		}
+
+
+		static Bitmap BitmapCreate(AssetManager assetManager, string assetName)
+		{
+			using (var str = assetManager.Open(assetName))
+			{
+				return BitmapFactory.DecodeStream(str);
+			}
+		}
+	}
+}
+
